aae11e2a0adbd2f9f40c5c79442e0255336a4db6
Vipin <85486070+vipinkhatri8@users.noreply.github.com>
2025-07-15 18:59:15 +0530

    15-07-2025 Evening 2

--- app/src/main/java/com/bussiness/slodoggiesapp/navigation/NavGraph.kt
+++ app/src/main/java/com/bussiness/slodoggiesapp/navigation/NavGraph.kt
@@ -5,7 +5,6 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.bussiness.slodoggiesapp.ui.intro.SplashScreen
-import com.bussiness.slodoggiesapp.ui.screens.commonscreens.OnboardingScreen
 import com.bussiness.slodoggiesapp.ui.screens.petowner.LocationPermissionScreen
 import com.bussiness.slodoggiesapp.ui.screens.petowner.NotificationPermissionScreen
 import com.bussiness.slodoggiesapp.ui.screens.petowner.PetMainScreen
