9a5e50543de67183253c328d0ea5f1400a16a7fc
DeepakYesIt <yesitlabs.deepaka@gmail.com>
2025-11-21 17:47:48 +0530

    sfggwg
    fgbtrb

--- app/src/main/java/com/bussiness/slodoggiesapp/activity/MainActivity.kt
+++ app/src/main/java/com/bussiness/slodoggiesapp/activity/MainActivity.kt
@@ -46,7 +46,6 @@ class MainActivity : ComponentActivity() {
                 val isLoading by loaderViewModel.isLoading.collectAsState()
                 Box(Modifier.fillMaxSize()) {
                     NavGraph(navController = mainNavController)
-
                     LoaderOverlay(isVisible = isLoading)
                 }
             }
